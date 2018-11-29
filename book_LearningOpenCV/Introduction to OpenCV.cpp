b9258501cbf0060265bad4e9fc7987d58a590ed5
Muriel <17842868+MurielSpot@users.noreply.github.com>
2018-11-29 14:36:07 +0800

    Update Introduction to OpenCV.cpp

--- book_LearningOpenCV/Introduction to OpenCV.cpp
+++ book_LearningOpenCV/Introduction to OpenCV.cpp
@@ -1,6 +1,5 @@
 /*
-Getting Started  16
-First Program—Display a Picture  16
+1-2: First Program—Display a Picture  16
 Second Program—AVI Video  18
 Moving Around  19
 A Simple Transformation  22
@@ -10,3 +9,26 @@ Writing to an AVI File  27
 Onward  29
 Exercises  29
 */
+// 1读取图片----------------------------------------------------------------------------------------
+#include"stdafx.h"
+#include<iostream>
+
+#include<highgui.h>
+
+using namespace cv;
+
+int main()
+{
+	IplImage* img = cvLoadImage("em.jpg");
+	cvNamedWindow("example1", CV_WINDOW_AUTOSIZE);
+	cvShowImage("example1", img);
+	cvWaitKey(0);
+	cvReleaseImage(&img);
+	cvDestroyWindow("example1");
+	return 0;
+}
+
+// 2读取视频----------------------------------------------------------------------------------------
+
+
+// 3----------------------------------------------------------------------------------------
