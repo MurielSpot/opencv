77a5dc85740390faff099d4341cf1aceab156ee8
Muriel <17842868+MurielSpot@users.noreply.github.com>
2018-11-29 14:34:08 +0800

    Create Introduction to OpenCV.cpp

---
+++ book_LearningOpenCV/Introduction to OpenCV.cpp
@@ -0,0 +1,12 @@
+/*
+Getting Started  16
+First Program—Display a Picture  16
+Second Program—AVI Video  18
+Moving Around  19
+A Simple Transformation  22
+A Not-So-Simple Transformation  24
+Input from a Camera  26
+Writing to an AVI File  27
+Onward  29
+Exercises  29
+*/
